f0efdf3c629b9151252c081b788b4918156666a6
MrDefaults <88251253+MrDefaults@users.noreply.github.com>
2022-03-12 12:38:26 +0700

    Add files via upload

--- src/main/java/com/stand/Listener/PlayerOptionalListener.java
+++ src/main/java/com/stand/Listener/PlayerOptionalListener.java
@@ -2,7 +2,9 @@ package com.stand.Listener;
 
 import com.stand.PluginCollection;
 import com.stand.Utility.Common;
+import com.stand.Utility.PlayerUtil;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -11,15 +13,34 @@ public class PlayerOptionalListener implements Listener {
 
 	@EventHandler
 	public void onCombat(final EntityDamageByEntityEvent event) {
-		final Player player = (Player) event.getDamager();
 
-		if(PluginCollection.getPvpPlayer(player)) {
-			if (event.getDamager() instanceof Player) {
-				if(event.getEntity() instanceof Player) {
+		// Condition #1 - PVP without projectile
+		if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
+			final Player damager = (Player) event.getDamager();
+
+
+			if (PluginCollection.getPvpPlayer(damager) && !PlayerUtil.hasPerm(damager , "PlayerModification.PvpBypass")) {
+				event.setCancelled(true);
+				damager.sendMessage(Common.colorize("&cThis world isn't allowed to pvp!"));
+			}
+
+		// Condition #2 - PVP with projectile
+		} else if (event.getDamager() instanceof Projectile && event.getEntity() instanceof Player) {
+
+			final Projectile projectile = (Projectile) event.getDamager();
+
+			if (projectile.getShooter() instanceof Player && event.getEntity() instanceof Player) {
+
+				final Player damager = (Player) projectile.getShooter();
+
+				if (PluginCollection.getPvpPlayer(damager) && !PlayerUtil.hasPerm(damager , "PlayerModification.PvpBypass")) {
 					event.setCancelled(true);
-					player.sendMessage(Common.colorize("&cThis world isn't allowed to pvp!"));
+					damager.sendMessage(Common.colorize("&cThis world isn't allowed to pvp!"));
 				}
+
 			}
 		}
 	}
+
+
 }
